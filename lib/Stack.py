9f32d08192c8af66fc0b5873d6e20c831f35cbc2
TJ Stifter <tjstifter@gmail.com>
2023-07-07 12:35:30 -0400

    passes test

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,41 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        if self.size() == 0 :
+            return True
+        else : False
+        
 
     def push(self, item):
-        pass
-
+        if self.full():
+            pass
+        else :
+            self.items.append(item)
+        
     def pop(self):
-        pass
+        if self.isEmpty() :
+            return None
+        else :
+            return self.items.pop()
 
     def peek(self):
         pass
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        if self.size() == self.limit :
+            return True
+        else :
+            return False
 
     def search(self, target):
-        pass
+        for ele in self.items :
+            if ele == target :
+                return self.size() - self.items.index(target) - 1
+        return -1
